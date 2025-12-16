826a1ab4c3d4165b63d31e64490e4230786e77aa
PatriciaGavloski <patricia.navaski.g@gmail.com>
2025-12-16 17:37:20 -0300

    Estudos Java

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[]args){
+        
+        var scanner = new Scanner(System.in);
+        System.out.println("Informe um número: ");
+        var number = scanner.nextInt();
+        var keepVerify = true;
+        while (keepVerify){
+            System.out.println("Informe o número para verificação: ");
+            var toVerify = scanner.nextInt();
+            if (toVerify < number){
+                System.out.printf("Infome um número maior que %s \n", number);
+                continue;
+            }
+            var result = toVerify % number;
+            keepVerify = result == 0;
+            System.out.printf("%s %% %s = %s \n", toVerify, number, result);
+
+        }
+     
+        }
+
+    }
+
+
+
